378961228b0f4024c9ebac4aea857589dbf4d86e
dullmatian <someone_6262@yahoo.co.jp>
2021-11-01 06:32:02 +0900

    added application

---
+++ app.py
@@ -0,0 +1,12 @@
+from bottle import route, jinja2_template as template, run, static_file
+
+@route('/static/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='./static')
+
+@route('/')
+def home():
+    return template("templates/lp.html")
+
+if __name__ == '__main__':
+    run(host='localhost', port=8080, reloader=True, debug=True)
